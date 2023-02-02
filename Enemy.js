aa4b3a90037d2a22b17f8ff3bcf874e5b874e8c7
Sean Haimovitch <112636398+Digvorzhak@users.noreply.github.com>
2023-02-02 09:42:15 +0200

    Update Enemy.js

--- Enemy.js
+++ Enemy.js
@@ -107,13 +107,37 @@ const initEnemies = () => {
   const monster1 = new Enemy(1, "Alien", 100);
 };
 
-function startDecider(player, monster) {
+// function startDecider(player, monster) {
+//   dices[0] = Math.trunc(Math.random() * 20) + 1;
+//   dices[1] = Math.trunc(Math.random() * 20) + 1;
+//   player.dex += dices[0];
+//   monster.dex += dices[1];
+//   if (player.dex > monster.dex) return player.dmgCalc(player, monster, true);
+//   if (player.dex < monster.dex) return monster.dmgCalc(monster, player, false);
+//   // console.log("draw");
+//   return startDecider(player, monster);
+// }
+
+function startDecider({ player, monster }) {
   dices[0] = Math.trunc(Math.random() * 20) + 1;
   dices[1] = Math.trunc(Math.random() * 20) + 1;
   player.dex += dices[0];
   monster.dex += dices[1];
-  if (player.dex > monster.dex) return player.dmgCalc(player, monster, true);
-  if (player.dex < monster.dex) return monster.dmgCalc(monster, player, false);
-  // console.log("draw");
+  if (player.dex > monster.dex) return fightCalc({ attacker: player, defender: monster });
+  if (player.dex < monster.dex) return fightCalc({ attacker: monster, defender: player });
   return startDecider(player, monster);
+}
+
+startDecider({ player: mihile, monster: gromp });
+console.log(dices);
+console.log(mihile);
+console.log(gromp);
+
+function fightCalc({ attacker, defender }) {
+  let attackPower = 0;
+  attackDice = Math.trunc(Math.random() * 20) + 1;
+  attackPower = (attacker.str + attackDice) * attacker.lvl - defender.def;
+  if (attackPower < 0) return console.log("");
+  defender.hp -= attackPower;
+  if (defender.hp > 0) return startDecider({ player: attacker, defender: monster });
 }
